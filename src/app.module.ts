192b47e03483196c9cbf48462351ae7c60aec2bc
Ilham <ilham.pirm@gmail.com>
2020-07-31 06:45:58 +0400

    Admin UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,14 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule} from '@nestjs/serve-static'; // New
+import { ServeStaticModule} from '@nestjs/serve-static';
+import * as express from 'express'
 import { join } from 'path'; // New
 
 @Module({
   imports: [
     // ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'client/dist')}),
-    ServeStaticModule.forRoot({rootPath: join(__dirname, '../client/dist')}),
+    ServeStaticModule.forRoot({rootPath: join(__dirname,'..','client/dist')}),
       AuthModule,
     UsersModule,
     MongooseModule.forRoot('mongodb+srv://admin:2587889e@cluster0.0uly1.mongodb.net/root?retryWrites=true&w=majority'),
