66bac0432264231856fbeafa556d3c933d10509a
Ilham <ilham.pirm@gmail.com>
2020-07-31 06:43:23 +0400

    Admin UI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,22 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {AuthModule} from './auth/auth.module';
-import {UsersModule} from './users/users.module';
-import {MongooseModule} from '@nestjs/mongoose';
-import {ServeStaticModule} from '@nestjs/serve-static'; // New
-import {join} from 'path'; // New
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule} from '@nestjs/serve-static'; // New
+import { join } from 'path'; // New
 
 @Module({
-    imports: [
-        ServeStaticModule.forRoot({rootPath: join(__dirname,'../../client/dist')}),
-        AuthModule,
-        UsersModule,
-        MongooseModule.forRoot('mongodb+srv://admin:2587889e@cluster0.0uly1.mongodb.net/root?retryWrites=true&w=majority'),
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    // ServeStaticModule.forRoot({rootPath: join(__dirname, '..', 'client/dist')}),
+    ServeStaticModule.forRoot({rootPath: join(__dirname, '../client/dist')}),
+      AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb+srv://admin:2587889e@cluster0.0uly1.mongodb.net/root?retryWrites=true&w=majority'),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
 
